83b441bc62c76e79aa220f9facc0688de9f31d4f
Uttam Kumar Saha <uttamsaha982@live.com>
2023-11-29 11:55:50 +0600

    fetch all room and delete room for admin functionality added

--- src/Components/Pages/Roles/AdminRole/Pages/AllRomsTable.js
+++ src/Components/Pages/Roles/AdminRole/Pages/AllRomsTable.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import swal from 'sweetalert';
 
-const ViewOrderTable = ({order, index, setAllOrderData}) => {
-    const { img, roomId, email, startDate, endDate, price } = order;
+const AllRoomsTable = ({room, index, setAllRooms}) => {
+    const { img, roomId, price, name } = room;
 
     const handleDeleteOrder = async (roomId) => {
         // alert(`Clicked on ${roomId}`)
@@ -14,18 +14,18 @@ const ViewOrderTable = ({order, index, setAllOrderData}) => {
           dangerMode: true,
         }).then((willDelete) => {
           if (willDelete) {
-            const url = `http://localhost:5000/api/v1/orders/delete-room-order/${roomId}`;
+            const url = `http://localhost:5000/api/v1/products/rooms/${roomId}`;
             fetch(url, {
               method: "DELETE",
             })
               .then((res) => res.json())
               .then((data) => console.log(data));
-            swal("The order is Deleted", {
+            swal("The Room is Deleted", {
               icon: "success",
             });
     
             //this second fetched is use to refresh delete data 
-            fetch("http://localhost:5000/api/v1/orders/room-orders").then(res => res.json()).then(data => setAllOrderData(data?.data))
+            fetch("http://localhost:5000/api/v1/orders/room-orders").then(res => res.json()).then(data => setAllRooms(data?.data))
           } else {
             swal("Oder not deleted. You canceled it!");
           }
@@ -38,23 +38,21 @@ const ViewOrderTable = ({order, index, setAllOrderData}) => {
       <img className="w-28 h-20 rounded " src={img} alt="" />
     </td>
     <td>{roomId}</td>
-    <td>{email}</td>
-    <td>{startDate}</td>
-    <td>{endDate}</td>
+    <td>{name}</td>
     <td>{price}TK</td>
     <td>
       <button
         onClick={() => {
           handleDeleteOrder(roomId);
         }}
-        className="btn btn-error text-base-200"
+        className="btn btn-error btn-sm btn-outline hover:text-base-200"
       >
-        Delete Order
+        Delete Room
       </button>
-      <button className='btn btn-success ml-2'>Update Order</button>
+      <button className='btn btn-success btn-sm btn-outline ml-2'>Update Room</button>
     </td>
   </tr>
   )
 }
 
-export default ViewOrderTable;
+export default AllRoomsTable;
--- src/Components/Pages/Roles/AdminRole/Pages/AllRooms.js
+++ src/Components/Pages/Roles/AdminRole/Pages/AllRooms.js
@@ -1,46 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import AllRoomsTable from "./AllRomsTable";
 
 const AllRooms = () => {
+  const [allRooms, setAllRooms] = useState([])
+  // console.log(orderData)
+
+  useEffect(()=>{
+    fetch("http://localhost:5000/api/v1/products/rooms").then(res => res.json()).then(data => setAllRooms(data?.data))
+  },[])
   return (
     <div>
-        <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead className='bg-base-200'>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <th>1</th>
-        <td>Cy Ganderton</td>
-        <td>Quality Control Specialist</td>
-        <td>Blue</td>
-      </tr>
-      {/* row 2 */}
-      <tr>
-        <th>2</th>
-        <td>Hart Hagerty</td>
-        <td>Desktop Support Technician</td>
-        <td>Purple</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <th>3</th>
-        <td>Brice Swyre</td>
-        <td>Tax Accountant</td>
-        <td>Red</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead className="bg-base-200">
+          <tr className="">
+              <th></th>
+              <th>Image</th>
+              <th>Room ID</th>
+              <th>Room Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+              allRooms.map((room, index) => <AllRoomsTable key={room?.roomId} room={room} index={index} setAllRooms={setAllRooms}></AllRoomsTable>)
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AllRooms;
